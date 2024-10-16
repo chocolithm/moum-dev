97c608aa9146a68509258caeb7a9a359fb9a8542
Nari-Lee <serika9009@gmail.com>
2024-10-16 16:57:00 +0900

    gradle 프로젝트 생성

---
+++ src/test/java/moum/project/moum/MoumApplicationTests.java
@@ -0,0 +1,13 @@
+package moum.project.moum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoumApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
