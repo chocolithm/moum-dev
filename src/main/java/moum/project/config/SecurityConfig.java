6976ce39d6062b7f1db4219ce857dba6543f5bac
Nari-Lee <serika9009@gmail.com>
2024-10-16 17:16:26 +0900

    스프링 시큐리티 샘플 추가

---
+++ src/main/java/moum/project/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package moum.project.config;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * packageName    : moum.project.config
+ * fileName       : SecurityConfig
+ * author         : narilee
+ * date           : 24. 10. 15.
+ * description    : 애플리케이션의 보안 설정을 정의하는 구성 클래스입니다.
+ *                  이 클래스는 Spring Security 관련 설정을 담당합니다.
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 10. 15.        narilee       최초 생성
+ */
+@Configuration
+@AllArgsConstructor
+@Log4j2
+public class SecurityConfig {
+}
---
+++ src/main/java/moum/project/controller/SampleController.java
@@ -0,0 +1,42 @@
+package moum.project.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * packageName    : moum.project.controller
+ * fileName       : SampleController
+ * author         : narilee
+ * date           : 24. 10. 15.
+ * description    : 이 클래스는 웹 애플리케이션에서 접근 수준을 처리하는 컨트롤러입니다.
+ *                  모든 사용자, 회원, 관리자를 위한 다양한 매핑을 보여줍니다.
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 10. 15.        narilee       최초 생성
+ */
+@Controller
+@Log4j2
+@RequestMapping("/sample")
+public class SampleController {
+
+  // 로그인을 하지 않은 사용자도 접근 할수 있는 경로
+  @GetMapping("/all")
+  public void exAll(){
+    log.info("exAll..........");
+  }
+
+  // 로그인한 사용자만이 접근할 수 있는 경로
+  @GetMapping("/member")
+  public void exMember(){
+    log.info("exMember...........");
+  }
+
+  // 관리자 권한이 있는 사용자만이 접근할 수 있는 경로
+  @GetMapping("/admin")
+  public void exAdmin(){
+    log.info("exAdmin...........");
+  }
+}
