bfd082d46c5aec4553e9d0da9bf2b883e241b3a5
Nari-Lee <serika9009@gmail.com>
2024-10-17 16:43:27 +0900

    vo에 Lombok의 @Getter, @Setter 적용

--- src/main/java/moum/project/vo/User.java
+++ src/main/java/moum/project/vo/User.java
@@ -1,14 +1,20 @@
 package moum.project.vo;
 
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
-@Setter
-@Getter
-public class User implements Serializable {
+@Data
+public class User implements UserDetails, Serializable {
     private static final long serialVersionUID = 1L;
     private int no;
     private String nickname;
@@ -20,4 +26,34 @@ public class User implements Serializable {
         this.no = no;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return admin ? Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN"))
+                     : Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
