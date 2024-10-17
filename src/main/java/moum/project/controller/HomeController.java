41e9e861833dffeafdc061a3ffe4c3322b559b89
Nari-Lee <serika9009@gmail.com>
2024-10-17 13:16:17 +0900

    주석 추가, /home을 제외한 다른 url 접속 불가능시 gradle 빌드를 claen 후 다시 build 해주세요.

--- src/main/java/moum/project/controller/HomeController.java
+++ src/main/java/moum/project/controller/HomeController.java
@@ -3,14 +3,37 @@ package moum.project.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**
+ * packageName    : moum.project.controller
+ * fileName       : HomeController
+ * author         : narilee
+ * date           : 24. 10. 16.
+ * description    : 홈페이지와 관련된 요청을 처리하는 컨트롤러 클래스입니다.
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 24. 10. 15.        narilee       최초 생성
+ * 24. 10. 17.        narilee       테스트용 순수 HTML인 home2로 뷰 추가
+ */
 @Controller
 public class HomeController {
 
+  /**
+   * 이 메서드는 "/home" URL로 들어오는 GET 요청을 처리합니다.
+   * 현재 접속 불가능
+   *
+   * @return "home" 뷰 이름을 반환합니다.
+   */
   @GetMapping( "/home")
   public String home() {
     return "home";
   }
 
+  /**
+   * 이 메서드는 테스트용으로 "/home2" URL로 들어오는 GET 요청을 처리합니다.
+   *
+   * @return 순수 html인 "home2" 뷰 이름을 반환합니다.
+   */
   @GetMapping( "/home2")
   public String home2() {
     return "home2";
