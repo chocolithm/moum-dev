b4bc7062c4189edf997ef263b22fc57b73a75f96
chocolithm <kys8336@gmail.com>
2024-10-21 10:47:25 +0900

    SecurityConfig 주석 해제

--- src/main/java/moum/project/config/SecurityConfig.java
+++ src/main/java/moum/project/config/SecurityConfig.java
@@ -27,21 +27,21 @@ import org.springframework.security.web.SecurityFilterChain;
 @Log4j2
 public class SecurityConfig {
 
-//  @Bean
-//  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//    http
-//        .authorizeHttpRequests((requests) -> requests
-//            .requestMatchers("/", "/home", "/css/**", "/js/**", "/images/**").permitAll()
-//            .anyRequest().authenticated()
-//        )
-//        .formLogin((form) -> form
-//            .loginPage("/login")
-//            .permitAll()
-//        )
-//        .logout(LogoutConfigurer::permitAll);
-//
-//    return http.build();
-//  }
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .authorizeHttpRequests((requests) -> requests
+            .requestMatchers("/", "/home", "/css/**", "/js/**", "/images/**").permitAll()
+            .anyRequest().authenticated()
+        )
+        .formLogin((form) -> form
+            .loginPage("/login")
+            .permitAll()
+        )
+        .logout(LogoutConfigurer::permitAll);
+
+    return http.build();
+  }
 
 //  @Bean
 //  public PasswordEncoder passwordEncoder() {
