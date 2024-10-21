917087be102b5e2c13a9d40ad4df06374b3d93da
Homez96 <polabear0802@naver.com>
2024-10-21 19:40:46 +0900

    업적 list 추가

--- src/main/java/moum/project/controller/AchievementController.java
+++ src/main/java/moum/project/controller/AchievementController.java
@@ -23,16 +23,38 @@ public class AchievementController {
   private final AchievementService achievementService;
 
   @GetMapping("list")
-  @ResponseBody
-  public Object list(Model model) throws Exception {
-    return achievementService.list();
+  public String list(Model model) throws Exception {
+    List<Achievement> list = achievementService.list();
+    model.addAttribute("list", list); //모델에다가 업적 정보를 가진 list를 list라는 이름으로 담는다.
+    return "achievement/list";
   }
+
 
   @GetMapping("view")
   @ResponseBody
-  public Object view(String photo) throws Exception {
-    return achievementService.get(photo); // 상세 페이지 뷰 반환
+  public Object view(String id) throws Exception {
+    return achievementService.get(id); // 상세 페이지 뷰 반환
   }
 
+  @GetMapping("delete")
+  @ResponseBody
+  public Object delete(String id) throws Exception {
+    achievementService.delete(id);
+    return "Achievement with id" + id + "삭제되었습니다.";
+  }
+
+  @PostMapping("add")
+  @ResponseBody
+  public Object add(@RequestBody Achievement achievement) throws Exception {
+    achievementService.add(achievement);
+    return achievementService.get(achievement.getId());
+  }
+
+  @PostMapping("update")
+  @ResponseBody
+  public Object update(@RequestBody Achievement achievement) throws Exception {
+    achievementService.update(achievement);
+    return achievementService.get(achievement.getId());
+  }
 
 }
