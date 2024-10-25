b9738b58f3ce958daf9bb111c78845922a599221
stereokim123 <rarangoon@gmail.com>
2024-10-25 14:53:52 +0900

    댓글 저장 기능 날라가서 다시 수정

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -457,42 +457,84 @@ function fetchChat(chatroomNo) {
 
 
 
-// 댓글 길이 카운팅
-function countingLength(content) {
-    if (content.value.length > 300) {
-        alert('댓글을 300자 이하로 입력해 주세요.');
-        content.value = content.value.substring(0, 300);
-        content.focus();
-    }
-    document.getElementById('counter').innerText = content.value.length + '/300자';
-}
-
-
 // 댓글 저장
-function saveComment() {
+    function saveComment(boardNo) {
 
-    const content = document.getElementById('content');
-    isValid(content, '댓글');
+        const content = document.getElementById('content');
+        // isValid(content, '댓글');
 
-    const boardNo = [[`${board.no}`]];
-    const params = {
-        boardNo: boardId,
-        content: content.value,
-        writer: '홍길동'
-    }
-
-    $.ajax({
-        url: `/board/${boardNo}/comments`,
-        type: 'post',
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json',
-        data: JSON.stringify(params),
-        async: false,
-        success: function (response) {
-            console.log(response);
-        },
-        error: function (request, status, error) {
-            console.log(error)
+        // const boardNo = [[ `${board.no}` ]];
+        // const boardNo = [[ `${board.no}` ]];
+        const params = {
+            boardNo : boardNo,
+            content : content.value,
+            // writer : '홍길동',
+            userNo : 5
         }
-    })
-}
+
+        const header = $("meta[name='_csrf_header']").attr('content');
+        const token = $("meta[name='_csrf']").attr('content');
+        $.ajax({
+            url : `/Comment/board/${boardNo}/comments`,
+            type : 'post',
+            contentType : 'application/json; charset=utf-8',
+            dataType : 'json',
+            data : JSON.stringify(params),
+            async : false,
+            beforeSend: function(xhr){
+                xhr.setRequestHeader(header, token);
+            },
+            success : function (response) {
+                alert('댓글 등록 완료.');
+                content.value = '';
+                document.getElementById('counter').innerText = '0/300자';
+                findAllComment(boardNo);
+            },
+            error : function (request, status, error) {
+                console.log(error)
+            }
+        })
+    }
+// 전체 댓글 조회
+    function findAllComment(boardNo) {
+        // const postId = [[ ${board.no} ]];
+        $.ajax({
+            url : `/Comment/board/${boardNo}/comments`,
+            type : 'get',
+            dataType : 'json',
+            async : false,
+            success : function (response) {
+                console.log(response);
+                // 1. 조회된 데이터가 없는 경우
+                if ( !response.length ) {
+                    document.querySelector('.cm_list').innerHTML = '<div class="cm_none"><p>등록된 댓글이 없습니다.</p></div>';
+                    return false;
+                }
+                // 2. 렌더링 할 HTML을 저장할 변수
+                let commentHtml = '';
+                // 3. 댓글 HTML 추가
+                response.forEach(row => {
+                    commentHtml += `
+                        <div>
+                            
+                            <div class="cont"><div class="txt_con">${row.content}</div></div>
+                            <p class="func_btns">
+                                <button type="button" class="btns"><span class="icons icon_modify">수정</span></button>
+                                <button type="button" class="btns"><span class="icons icon_del">삭제</span></button>
+                            </p>
+                        </div>
+                    `;
+                })
+                // <span class="writer_img"><img src="/images/default_profile.png" width="30" height="30" alt="기본 프로필 이미지"/></span>
+                // <p class="writer">
+                //     <em>${row.writer}</em>
+                //     <span class="date">${dayjs(row.createdDate).format('YYYY-MM-DD HH:mm')}</span>
+                // </p>
+                // 4. class가 "cm_list"인 요소를 찾아 HTML을 렌더링
+                document.querySelector('.cm_list').innerHTML = commentHtml;
+            },
+            error : function (request, status, error) {
+                console.log(error)
+            }
+        })
+    }
