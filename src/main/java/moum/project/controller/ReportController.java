478f16a8fa1677a82da311854d1c54d2de6fe920
j1yunn <yangjiyun1107@gmail.com>
2024-11-01 15:20:22 +0900

    신고 리스트 생성

--- src/main/java/moum/project/controller/ReportController.java
+++ src/main/java/moum/project/controller/ReportController.java
@@ -3,11 +3,20 @@ package moum.project.controller;
 import lombok.RequiredArgsConstructor;
 import moum.project.service.ReportService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/report")
 public class ReportController {
 
   private final ReportService reportService;
 
+  @GetMapping("/list")
+  public String list() {
+    
+    return "report/list";
+
+  }
 }
