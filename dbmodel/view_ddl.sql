bde50004c26ddbf0f0985e66da375a1dbf360dd3
chocolithm <kys8336@gmail.com>
2024-11-02 12:52:38 +0900

    user_achievement_rank view 생성 / 회원-업적 dummy 수정

---
+++ dbmodel/view_ddl.sql
@@ -0,0 +1,14 @@
+-- 회원별 업적점수 랭킹
+CREATE VIEW user_achievement_rank
+AS select
+	rank() OVER (ORDER BY sum(a.score) desc) AS userrank,
+	ua.user_id AS user_id,
+	u.nickname AS nickname,
+	sum(a.score) AS score
+from
+	user_achievement ua
+	left join achievement a on ua.achievement_id = a.achievement_id
+	left join user u on ua.user_id = u.user_id
+where ua.progress = 100
+group by ua.user_id
+order by score desc;
