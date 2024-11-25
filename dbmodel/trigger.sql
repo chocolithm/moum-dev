98b581a34681bac12e194ae640291acd8eb7906c
chocolithm <kys8336@gmail.com>
2024-11-25 15:02:28 +0900

    trigger 파일 재커밋

--- dbmodel/trigger.sql
+++ dbmodel/trigger.sql
@@ -5,7 +5,8 @@ AFTER INSERT ON user
 FOR EACH ROW
 BEGIN
     INSERT INTO user_achievement (user_id, achievement_id)
-    SELECT NEW.id, id FROM achievement;
+    SELECT NEW.user_id, achievement_id
+    FROM achievement;
 END$$
 
 DELIMITER ;
