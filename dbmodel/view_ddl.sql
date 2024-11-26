83deffed46f8b13ccb4459f22c2a8d37bc5b8956
chocolithm <kys8336@gmail.com>
2024-11-26 21:20:48 +0900

    collection_photo 테이블에 is_primary 대표사진 컬럼 추가

--- dbmodel/view_ddl.sql
+++ dbmodel/view_ddl.sql
@@ -15,10 +15,3 @@ where
 		and u.end_date is null
 group by ua.user_id
 order by score desc;
-
--- user_achievement에 있는 max_count 컬럼은 achievement 테이블로 이동
--- trade_board 테이블에 trade_type 컬럼 추가
--- alert 테이블에 alert_category_id 컬럼 varchar(50)으로 수정
--- user_restricted 테이블에 comment 컬럼 추가
--- collection 테이블에 is_deleted 컬럼 추가
--- maincategory에 color 컬럼 추가
